9369db81bff7ded1dd268dff32c822f45bd8abdb
Juan Morales <launchx10261@innovaccion.mx>
2022-04-25 17:28:38 -0500

    Terminando de crear las pruebas con TDD y confirmando que funciona

--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,17 @@
-describe("Test Suite Dummy Description", () => {
-    test('Case 1 Dummy', () => {
-      const resultOfSomething = 1 + 2
-      expect(resultOfSomething).toBe(3);
+const Spiderman = require('./../app/Spiderman')
+
+describe("Unit test for Spiderman Class", () => {
+    test('1) Create an Spiderman Class', () => {
+      const andrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield",2,"Sony")
+      expect(andrewGarfield.name).toBe("Spiderman Sony");
+      expect(andrewGarfield.age).toBe(31);
+      expect(andrewGarfield.actor).toBe("Andrew Garfield");
+      expect(andrewGarfield.movies).toBe(2);
+      expect(andrewGarfield.studio).toBe("Sony");
     });
-  })
+
+    test('2) Create an Spiderman Class with methods', () => {
+    const andrewGarfield = new Spiderman("Spiderman Sony",31,"Andrew Garfield",2,"Sony")
+    expect(andrewGarfield.getInfo()).toBe("Hi my name is Andrew Garfield and I'm from Sony Studios");
+    });
+})
